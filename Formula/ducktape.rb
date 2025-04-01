ba43029de5056d65ad9b38c29c5586359459685e
Shaun S <shaun.stuart@gmail.com>
2025-04-01 18:00:34 +1300

    Bump Ducktape version to 0.1.7 and update SHA256 checksum

--- Formula/ducktape.rb
+++ Formula/ducktape.rb
@@ -1,9 +1,9 @@
 class Ducktape < Formula
   desc "AI-powered terminal tool for Apple Calendar, Reminders and Notes"
   homepage "https://github.com/DuckTapeAI/ducktape"
-  version "0.1.6"
+  version "0.1.7"
   url "https://github.com/DuckTapeAI/ducktape/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "13a84536cc215e7bf096b6d2ba3197bcc64e378085d1bf73c451be3bcd199f0e"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   
   depends_on "rust" => :build
